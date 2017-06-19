3dd3866d2df7f1adf3614f595ad71fd4b8176cb9
kcs93023 <kcs93023@gmail.com>
2017-06-19 16:49:08 +0900

    Add Company Select

--- src/Paser.java
+++ src/Paser.java
@@ -6,13 +6,33 @@
 public class Paser {
     private String address;
     private String filename;
-    Paser(String domainName){
+
+    Paser(String domainName) {
         init(domainName);
     }
 
     private void init(String domainName) {
-
-        Crawler crawler = new Crawler(address,filename);
+        switch (domainName) {
+            case "Naver":
+                address = "https://www.naver.com";
+                filename = "Naver";
+            case "Chosun":
+                address = "http://www.chosun.com";
+                filename = "Chosun";
+            case "Donga":
+                address = "http://www.donga.com";
+                filename = "Donga";
+            case "Joongang":
+                address = "http://joongang.joins.com";
+                filename = "Joongang";
+            case "Mail":
+                address = "http://www.mk.co.kr";
+                filename = "Mail";
+            case "NewYorkTimes":
+                address = "https://www.nytimes.com";
+                filename = "Newyork";
+        }
+        Crawler crawler = new Crawler(address, filename);
         crawler.start();
     }
 }
