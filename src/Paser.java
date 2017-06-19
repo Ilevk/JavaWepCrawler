4f6b11bad3cb36b29bfc62d332eab3dbbbf2f67f
kcs93023 <kcs93023@gmail.com>
2017-06-19 16:42:40 +0900

    Add TestMain class And Divide Parser class into TestMain & Parser class

--- src/Paser.java
+++ src/Paser.java
@@ -4,9 +4,15 @@
  * Created by kyle on 2017. 6. 19..
  */
 public class Paser {
+    private String address;
+    private String filename;
+    Paser(String domainName){
+        init(domainName);
+    }
 
-    public static void main(String[] args){
-        Crawler crawler = new Crawler("https://www.naver.com/","Naver");
+    private void init(String domainName) {
+
+        Crawler crawler = new Crawler(address,filename);
         crawler.start();
     }
 }
---
+++ src/TestMain.java
@@ -0,0 +1,9 @@
+/**
+ * Created by kyle on 2017. 6. 19..
+ */
+public class TestMain {
+    public static void main(String[] args){
+      Paser paser = new Paser("Naver");
+
+    }
+}
